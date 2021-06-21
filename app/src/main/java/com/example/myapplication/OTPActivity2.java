bceacca5d6e5f55d834a70e9011318391eeb300e
Hrishikesh Kulkarni <hmkulkarni2001@gmail.com>
2021-06-21 20:30:02 +0530

    OTP Activity 2 page done

---
+++ app/src/main/java/com/example/myapplication/OTPActivity2.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OTPActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otp2);
+    }
+
+}
