fbe8d7ac0e8b8c443790950082dd63cc768343f6
anjali301 <73717496+anjali301@users.noreply.github.com>
2021-07-06 00:43:35 +0530

    login register

--- app/src/main/java/com/example/myapplication/LoginPage.java
+++ app/src/main/java/com/example/myapplication/LoginPage.java
@@ -1,20 +1,54 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 public class LoginPage extends AppCompatActivity {
+    protected EditText edUsername, edPassword; //
+    protected Button btnLogin;
+
+    private final String CREDENTIAL_SHARED_PREF = "our_shared_pref"; //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edUsername = findViewById(R.id.editTextTextPersonName);  //
+        edPassword = findViewById(R.id.editTextTextPassword);//
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        Button loginButton = findViewById(R.id.reg_btn);
+        btnLogin = findViewById(R.id.reg_btn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences credential = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
+                String strUsername = credential.getString("Username", null);
+                String strPassword = credential.getString("Password", null);
+
+                String username_from_ed = edUsername.getText().toString();
+                String password_from_ed = edPassword.getText().toString();
+
+                if (strUsername != null && username_from_ed != null &&  strUsername.equalsIgnoreCase(username_from_ed)){
+                    if (strPassword != null && password_from_ed != null && strPassword.equalsIgnoreCase(password_from_ed)){
+                        Toast.makeText(LoginPage.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginPage.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginPage.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
     public void proceedMainPage(View view) {
         Intent i = new Intent(this, homepage.class);
@@ -24,5 +58,6 @@ public class LoginPage extends AppCompatActivity {
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
     }
-    
+
+
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,17 +11,40 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
-    protected EditText username, businessName, password, confirmPass;
-    protected Button registerButton;
+    protected EditText edUsername, businessName, edPassword, edConfirmPassword;
+    protected Button btnCreateUser;
+
+    private final String CREDENTIAL_SHARED_PREF = "our_shared_pref";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
-        username = findViewById(R.id.editTextTextPersonName);
+        edUsername = findViewById(R.id.editTextTextPersonName);
         businessName = findViewById(R.id.editTextTextPersonName2);
-        password = findViewById(R.id.editTextTextPassword);
-        confirmPass = findViewById(R.id.editTextTextPassword3);
-        registerButton = findViewById(R.id.reg_btn);
+        edPassword = findViewById(R.id.editTextTextPassword);
+        edConfirmPassword = findViewById(R.id.editTextTextPassword3);
+        btnCreateUser = findViewById(R.id.reg_btn);
+
+        btnCreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String strPassword = edPassword.getText().toString();
+                String strConfirmPassword = edConfirmPassword.getText().toString();
+                String strUsername = edUsername.getText().toString();
+
+                if (strPassword != null && strConfirmPassword != null && strPassword.equalsIgnoreCase(strConfirmPassword)){
+                    SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = credentials.edit();
+                    editor.putString("Password", strPassword);
+                    editor.putString("Username", strUsername);
+                    editor.commit();
+
+                    RegisterActivity.this.finish();
+                }
+            }
+        });
+
     }
     public void proceedOTP(View view) {
         startActivity(new Intent(this, LoginPage.class));
